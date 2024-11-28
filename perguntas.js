e497d22aa2ac351419673ef5a6c1713231a30beb
japinhaharuu <00001098284525sp@al.educacao.sp.gov.br>
2024-11-28 20:35:48 -0300

    Final

---
+++ perguntas.js
@@ -0,0 +1,29 @@
+criaCartao(
+    'Geografia',
+    'Qual é a capital dos Estados Unidos?',
+    'A capital dos Estados Unidos é Washington, D.C.'
+)
+
+criaCartao(
+    'História',
+    'Em que ano foi proclamada a independência do Brasil?',
+    'A independência do Brasil foi proclamada em 1822.'
+)
+
+criaCartao(
+    'Geografia',
+    'Quais são os três estados que fazem fronteira com o Brasil?',
+    'Os três estados que fazem fronteira com o Brasil são Argentina, Paraguai e Bolívia.'
+)
+
+criaCartao(
+    'Geografia',
+    'Quantos estados tem o Brasil?',
+    'O Brasil tem 26 estados e 1 Distrito Federal.'
+)
+
+criaCartao(
+    'História',
+    'Quem foi o primeiro presidente dos Estados Unidos?',
+    'O primeiro presidente dos Estados Unidos foi George Washington.'
+)
